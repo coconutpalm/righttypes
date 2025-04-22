c54a7796cb005ad9a4b8486c6f2db79dc555d495
David Orme <djo@coconut-palm-software.com>
2025-04-22 17:19:01 -0500

    Tidy up debug utilities

--- src/righttypes/util/debug.clj
+++ src/righttypes/util/debug.clj
@@ -2,22 +2,6 @@
   "Some simple utilities for debugging function internals."
   (:require [clojure.pprint :as pp]))
 
-
-(defn spy
-  "pretty-prints `x`, then returns it."
-  [x]
-  (pp/pprint x)
-  x)
-
-
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defmacro spy->>
-  "Like thread-last, but inserts a (spy) in between each step"
-  [& steps]
-  (let [new-steps (interpose `(spy) steps)]
-    `(->> ~@new-steps)))
-
-
 (defmacro local-bindings
   "Produces a map of the names of local bindings to their values.
    For now, strip out gensymed locals."
@@ -25,3 +9,26 @@
   (let [symbols (remove #(.contains (str %) "_")
                         (map key @clojure.lang.Compiler/LOCAL_ENV))]
     (zipmap (map (fn [sym] `(quote ~sym)) symbols) symbols)))
+
+(def ^{:doc "The function used to print a value when debugging.
+             Defaults to `pprint`.  `tap>` is another useful option."
+       :dynamic true} 
+  *spy-fn* pp/pprint)
+
+(defn spy
+  "emits `x` using `*spy-fn*`, then returns it."
+  [x]
+  (*spy-fn* x)
+  x)
+
+(defmacro spy->>
+  "Like thread-last, but inserts a `(spy)` call in between each step"
+  [& steps]
+  (let [new-steps (interpose `(spy) steps)]
+    `(->> ~@new-steps)))
+
+(defmacro spy->
+  "Like thread-first, but inserts a `spy` call in between each step"
+  [& steps]
+  (let [new-steps (interpose 'spy steps)]
+    `(-> ~@new-steps)))
